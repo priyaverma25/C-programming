66d74c2ff8aaf3bf43e71180e61b83c167ca27ea
priyaverma25 <priyaknipss25@gmail.com>
2026-04-13 20:55:19 +0530

    nuber pattern problem

--- 01.Basis/basis-1.cpp
+++ 01.Basis/basis-1.cpp
@@ -7,12 +7,21 @@ int main() {
 
 
     // ----------------Triangle Pattern---------
-    int n=5;
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<i+1; j++) {
-            cout <<"*";
+    // int n=5;
+    // for(int i=0; i<n; i++) {
+    //     for(int j=0; j<i+1; j++) {
+    //         cout <<"*";
+    //     }
+    //     cout << endl;
+    // }
+
+    int i,j;
+
+    for(i=1; i <= 4; i++) {
+        for(j = 1; j <= 1; j++) {
+            printf("%d", j);
         }
-        cout << endl;
+        printf("\n");
     }
 
     return 0;
