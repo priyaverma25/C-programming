d9731b7e1484e83731f16632585d4101f4b822de
priyaverma25 <priyaknipss25@gmail.com>
2026-04-21 22:20:52 +0530

    Reverse Array problem

--- 01.Basis/basis-1.cpp
+++ 01.Basis/basis-1.cpp
@@ -95,21 +95,31 @@ int main() {
     
     //----------zig-zag pattern-------
      
-    int n = 9;
-    //columns
+    // int n = 9;
+     //columns
 
-    for(int i = 1; i <= 3; i++) {
-        for(int j = 1; j <= n; j++) {
-            if((i == 1 && j % 4 == 1)) ||
-            (i == 2 && j % 4 == 2) ||
-            (i == 3 && j % 4 == 3) {
-                cout << "*";
-            } else {
-                cout << " ";
-            }
-        }
-        cout << endl;
+    // for(int i = 1; i <= 3; i++) {
+    //     for(int j = 1; j <= n; j++) {
+    //         if((i == 1 && j % 4 == 1)) ||
+    //         (i == 2 && j % 4 == 2) ||
+    //         (i == 3 && j % 4 == 3) {
+    //             cout << "*";
+    //         } else {
+    //             cout << " ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+
+    //---------Reverse Array ---------
+
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    for(int i = 4; i >= 0; i--) {
+        cout << arr[i] << " ";
+
+        
     }
-
     return 0;
 };
