205f4819c60a8f7eed02acea17c79ff0b123b186
priyaverma25 <priyaknipss25@gmail.com>
2026-05-18 19:46:11 +0530

    Student record management system

--- 01.Basis/basis-1.cpp
+++ 01.Basis/basis-1.cpp
@@ -460,38 +460,60 @@ int main() {
 
     // --------Matrix Addition------
 
-    int a[2][2], b[2][2], sum[2][2];
+    // int a[2][2], b[2][2], sum[2][2];
 
-    cout << "Enter first matrix:\n";
+    // cout << "Enter first matrix:\n";
 
-    for(int i=0;i<2;i++) {
-        for(int j=0;j<2;j++) {
-            cin >> a[i][j];
-        }
-    }
+    // for(int i=0;i<2;i++) {
+    //     for(int j=0;j<2;j++) {
+    //         cin >> a[i][j];
+    //     }
+    // }
 
-    cout << "Enter second matrix:\n";
+    // cout << "Enter second matrix:\n";
 
-    for(int i=0;i<2;i++) {
-        for(int j=0;j<2;j++) {
-            cin >> b[i][j];
-        }
-    }
+    // for(int i=0;i<2;i++) {
+    //     for(int j=0;j<2;j++) {
+    //         cin >> b[i][j];
+    //     }
+    // }
 
-    for(int i=0;i<2;i++) {
-        for(int j=0;j<2;j++) {
-            sum[i][j] = a[i][j] + b[i][j];
-        }
-    }
+    // for(int i=0;i<2;i++) {
+    //     for(int j=0;j<2;j++) {
+    //         sum[i][j] = a[i][j] + b[i][j];
+    //     }
+    // }
 
-    cout << "Sum Matrix:\n";
+    // cout << "Sum Matrix:\n";
 
-    for(int i=0;i<2;i++) {
-        for(int j=0;j<2;j++) {
-            cout << sum[i][j] << " ";
-        }
-        cout << endl;
-    }
+    // for(int i=0;i<2;i++) {
+    //     for(int j=0;j<2;j++) {
+    //         cout << sum[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+
+    //------Student record management system--
+
+
+
+    string name;
+    int roll, marks;
+
+    cout << "Enter Name: ";
+    cin >> name;
+
+    cout << "Enter Roll Number: ";
+    cin >> roll;
+
+    cout << "Enter Marks: ";
+    cin >> marks;
+
+    cout << "\n--- Student Record ---\n";
+    cout << "Name: " << name << endl;
+    cout << "Roll No: " << roll << endl;
+    cout << "Marks: " << marks << endl;
 
     return 0;
 };
