2b333e484f6c68391d1c094689f5a1c1e34c188f
priyaverma25 <priyaknipss25@gmail.com>
2026-05-19 19:07:19 +0530

    Bubble short

--- 01.Basis/basis-1.cpp
+++ 01.Basis/basis-1.cpp
@@ -498,22 +498,47 @@ int main() {
 
 
 
-    string name;
-    int roll, marks;
+    // string name;
+    // int roll, marks;
 
-    cout << "Enter Name: ";
-    cin >> name;
+    // cout << "Enter Name: ";
+    // cin >> name;
 
-    cout << "Enter Roll Number: ";
-    cin >> roll;
+    // cout << "Enter Roll Number: ";
+    // cin >> roll;
 
-    cout << "Enter Marks: ";
-    cin >> marks;
+    // cout << "Enter Marks: ";
+    // cin >> marks;
 
-    cout << "\n--- Student Record ---\n";
-    cout << "Name: " << name << endl;
-    cout << "Roll No: " << roll << endl;
-    cout << "Marks: " << marks << endl;
+    // cout << "\n--- Student Record ---\n";
+    // cout << "Name: " << name << endl;
+    // cout << "Roll No: " << roll << endl;
+    // cout << "Marks: " << marks << endl;
+  
+    //-----Bubble short --
+
+    int a[5], i, j, temp;
+
+    printf("Enter 5 numbers:\n");
+
+    for(i = 0; i < 5; i++)
+        scanf("%d", &a[i]);
+
+    for(i = 0; i < 4; i++) {
+        for(j = 0; j < 4 - i; j++) {
+            if(a[j] > a[j + 1]) {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted Array:\n");
+
+    for(i = 0; i < 5; i++)
+        printf("%d ", a[i])
+
 
     return 0;
 };
